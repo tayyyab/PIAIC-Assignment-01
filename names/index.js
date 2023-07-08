acb0a49298a2d6c870dc39b8221f897179898f14
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-08 19:22:23 +0500

    011: Names

---
+++ names/index.js
@@ -0,0 +1,10 @@
+var names = [
+    "Amar",
+    "Nabeel",
+    "Fahad",
+    "Usama",
+];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
---
+++ names/index.ts
@@ -0,0 +1,11 @@
+let names = [
+    "Amar",
+    "Nabeel",
+    "Fahad",
+    "Usama",
+]
+
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
