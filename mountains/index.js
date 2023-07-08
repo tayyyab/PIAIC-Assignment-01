801d1bec15ea2ed31a4f64bca3ee3072b5be727e
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-08 20:49:37 +0500

    020: Mountains Array

---
+++ mountains/index.js
@@ -0,0 +1,5 @@
+var mountains = [];
+mountains.push("Mount Everest");
+mountains.push("K2");
+mountains.push("Aconcagua");
+console.log(mountains);
---
+++ mountains/index.ts
@@ -0,0 +1,7 @@
+let mountains : string[] = [];
+
+mountains.push("Mount Everest");
+mountains.push("K2");
+mountains.push("Aconcagua");
+
+console.log(mountains);
