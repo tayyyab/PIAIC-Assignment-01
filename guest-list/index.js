99d3be8b78b5427202e74a6e978fa05e6fbc459e
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-08 19:42:46 +0500

    014: Guest List

---
+++ guest-list/index.js
@@ -0,0 +1,6 @@
+var guest_list = [
+    "Imran Khan",
+    "Dr. Israr Ahmed",
+    "Lukman"
+];
+guest_list.forEach(function (e) { return console.log("Greeting! ".concat(e, ", I have heard many great thing about you and want to learn from you. That is why I am sending this invitation for the Dinner, Hope you attend. Thanks.")); });
---
+++ guest-list/index.ts
@@ -0,0 +1,7 @@
+let guest_list : string[] = [
+    "Imran Khan",
+    "Dr. Israr Ahmed",
+    "Lukman"
+];
+
+guest_list.forEach(e => console.log(`Greeting! ${e}, I have heard many great thing about you and want to learn from you. That is why I am sending this invitation for the Dinner, Hope you attend. Thanks.`));
