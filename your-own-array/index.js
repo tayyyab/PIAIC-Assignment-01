507109e6ba693c2e6aebeee671ad6cdf0f01d976
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-08 19:33:56 +0500

    013: Your own Array

---
+++ your-own-array/index.js
@@ -0,0 +1,6 @@
+var wishes = [
+    "I travel also 30 Km form home to offce everyday.",
+    "My current mode of transpotarion is CD 70 bike.",
+    "I would like to buy a honda 125 sport bike to make my ride more confortable.",
+];
+wishes.forEach(function (e) { return console.log(e); });
---
+++ your-own-array/index.ts
@@ -0,0 +1,7 @@
+let wishes = [
+    "I travel also 30 Km form home to offce everyday.",
+    "My current mode of transpotarion is CD 70 bike.",
+    "I would like to buy a honda 125 sport bike to make my ride more confortable.",
+];
+
+wishes.forEach(e => console.log(e));
