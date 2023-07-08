4b1286d291abf27d7f4499555687a3fb1b97fd79
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-08 19:27:55 +0500

    012: Greetings

---
+++ greetings/index.ts
@@ -0,0 +1,11 @@
+let names_2 = [
+    "Amar",
+    "Nabeel",
+    "Fahad",
+    "Usama",
+]
+
+console.log(`Hi ${names_2[0]}, How are you?`);
+console.log(`Hi ${names_2[1]}, How are you?`);
+console.log(`Hi ${names_2[2]}, How are you?`);
+console.log(`Hi ${names_2[3]}, How are you?`);
