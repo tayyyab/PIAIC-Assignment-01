0de18994e5d8b00c6a86e917c988f1ba13bd563e
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-08 19:13:24 +0500

    008: Update Number Eight

--- number-eight/index.ts
+++ number-eight/index.ts
@@ -1,6 +1,10 @@
-console.log('Addition: 4 + 4 = ', 4+4);
-console.log('Subtraction: 10 - 2 = ' ,10-2);
-console.log('Multiplication: 4 x 2 = ', 4*2);
-console.log('Division: 40 / 5 = ', 40 /5);
+// console.log('Addition: 4 + 4 = ', 4+4);
+// console.log('Subtraction: 10 - 2 = ' ,10-2);
+// console.log('Multiplication: 4 x 2 = ', 4*2);
+// console.log('Division: 40 / 5 = ', 40 /5);
 
 
+console.log(4+4);
+console.log(10-2);
+console.log(4*2);
+console.log(40 /5);
