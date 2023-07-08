266bf43dd48397b20af4652166297e0bb2a21b02
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-08 18:57:35 +0500

    005: Famous Quote 2

---
+++ famous-qoute-2/index.js
@@ -0,0 +1,3 @@
+var famous_person = "Lao Tzu";
+var message = "".concat(famous_person, " ones said, \"A journey of a thousand miles begins with a single step.\" ");
+console.log(message);
---
+++ famous-qoute-2/index.ts
@@ -0,0 +1,5 @@
+let famous_person: string = "Lao Tzu"
+
+let message : string = `${famous_person} ones said, \"A journey of a thousand miles begins with a single step.\" `;
+
+console.log(message);
