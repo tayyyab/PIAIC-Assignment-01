7f2366c836a4f50ed6fd3066c30230e0f0b66b4d
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-08 19:47:10 +0500

    015: Changing Guest List

--- guest-list/index.js
+++ guest-list/index.js
@@ -4,3 +4,7 @@ var guest_list = [
     "Lukman"
 ];
 guest_list.forEach(function (e) { return console.log("Greeting! ".concat(e, ", I have heard many great thing about you and want to learn from you. That is why I am sending this invitation for the Dinner, Hope you attend. Thanks.")); });
+console.log('The guest who can not attend: ', guest_list[0]);
+// Replacing the guest
+guest_list[0] = "Kizer";
+guest_list.forEach(function (e) { return console.log("Greeting! ".concat(e, ", I have heard many great thing about you and want to learn from you. That is why I am sending this invitation for the Dinner, Hope you attend. Thanks.")); });
--- guest-list/index.ts
+++ guest-list/index.ts
@@ -5,3 +5,10 @@ let guest_list : string[] = [
 ];
 
 guest_list.forEach(e => console.log(`Greeting! ${e}, I have heard many great thing about you and want to learn from you. That is why I am sending this invitation for the Dinner, Hope you attend. Thanks.`));
+
+console.log('The guest who can not attend: ', guest_list[0]);
+
+// Replacing the guest
+guest_list[0] = "Kizer"
+
+guest_list.forEach(e => console.log(`Greeting! ${e}, I have heard many great thing about you and want to learn from you. That is why I am sending this invitation for the Dinner, Hope you attend. Thanks.`));
