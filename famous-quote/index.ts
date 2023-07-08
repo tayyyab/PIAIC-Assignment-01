223e44500b69e4294b86d20557853dff40a1b565
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-08 19:18:56 +0500

    010: Adding few comments

--- famous-quote/index.ts
+++ famous-quote/index.ts
@@ -1,1 +1,2 @@
+// Name: Muhammad Tayyab Bahauddin 8-july-2023
 console.log("Lao Tzu ones said, \"A journey of a thousand miles begins with a single step.\"");
--- favorite-number/index.ts
+++ favorite-number/index.ts
@@ -1,3 +1,4 @@
+// What is your Favorite number?
 let fav_number: number = 14;
 
 console.log("My Favorite Number is ", fav_number);
