5d7d83a368aaed278b252ce3510724c689d72054
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-08 20:38:16 +0500

    018: Seeing the World

---
+++ see-the-world/index.ts
@@ -0,0 +1,18 @@
+let places: string[] = [
+    "Naran",
+    "Istambol",
+    "Reykjavik",
+    "Tokyo",
+    "Landon",
+];
+
+
+console.log(places);
+console.log([...places].sort());
+console.log(places);
+console.log([...places].sort().reverse());
+console.log(places);
+places.reverse();
+console.log(places);
+places.reverse();
+console.log(places);
