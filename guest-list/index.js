0dfab79d42f42cd558b8a8046c12cee68b3f3cdf
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-08 19:55:36 +0500

    016: More Guests

--- guest-list/index.js
+++ guest-list/index.js
@@ -8,3 +8,8 @@ console.log('The guest who can not attend: ', guest_list[0]);
 // Replacing the guest
 guest_list[0] = "Kizer";
 guest_list.forEach(function (e) { return console.log("Greeting! ".concat(e, ", I have heard many great thing about you and want to learn from you. That is why I am sending this invitation for the Dinner, Hope you attend. Thanks.")); });
+console.log('Greeting everyone, I have found a bigger table so let\'s invite few more people. ');
+guest_list.unshift('Alama Iqbal');
+guest_list.splice(3, 0, "Fahad Hassan");
+guest_list.push('Socrates');
+guest_list.forEach(function (e) { return console.log("Greeting! ".concat(e, ", I have heard many great thing about you and want to learn from you. That is why I am sending this invitation for the Dinner, Hope you attend. Thanks.")); });
--- guest-list/index.ts
+++ guest-list/index.ts
@@ -12,3 +12,13 @@ console.log('The guest who can not attend: ', guest_list[0]);
 guest_list[0] = "Kizer"
 
 guest_list.forEach(e => console.log(`Greeting! ${e}, I have heard many great thing about you and want to learn from you. That is why I am sending this invitation for the Dinner, Hope you attend. Thanks.`));
+
+
+console.log('Greeting everyone, I have found a bigger table so let\'s invite few more people. ');
+
+guest_list.unshift('Alama Iqbal');
+guest_list.splice(3, 0 , "Fahad Hassan")
+guest_list.push('Socrates');
+
+
+guest_list.forEach(e => console.log(`Greeting! ${e}, I have heard many great thing about you and want to learn from you. That is why I am sending this invitation for the Dinner, Hope you attend. Thanks.`));
