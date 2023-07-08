0c8cbff11ebf16bc4dd489e22ae781919c97b098
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-08 20:52:10 +0500

    021: Object Creation

--- mountains/index.js
+++ mountains/index.js
@@ -3,3 +3,9 @@ mountains.push("Mount Everest");
 mountains.push("K2");
 mountains.push("Aconcagua");
 console.log(mountains);
+var user = {
+    "name": "Tayyab",
+    "age": 27,
+    "designation": "Developer"
+};
+console.log(user);
--- mountains/index.ts
+++ mountains/index.ts
@@ -5,3 +5,11 @@ mountains.push("K2");
 mountains.push("Aconcagua");
 
 console.log(mountains);
+
+let user : object = {
+    "name": "Tayyab",
+    "age": 27,
+    "designation": "Developer"
+};
+
+console.log(user);
