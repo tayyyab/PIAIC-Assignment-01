bb23cd20e31a1769e4c1be9eb0087162489e567d
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-09 09:08:55 +0500

    028: Stages of Life

---
+++ stages-of-life/index.js
@@ -0,0 +1,19 @@
+var age = 2;
+if (age < 2) {
+    console.log("Person is baby.");
+}
+else if (age >= 2 && age < 4) {
+    console.log("Person is toddler.");
+}
+else if (age >= 4 && age < 13) {
+    console.log("Person is kid.");
+}
+else if (age >= 13 && age < 20) {
+    console.log("Person is teenager.");
+}
+else if (age >= 20 && age < 65) {
+    console.log("Person is adult.");
+}
+else if (age >= 65) {
+    console.log("Person is elder.");
+}
