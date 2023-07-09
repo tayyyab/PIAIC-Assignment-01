64cf1c5cf45e78e10685acb76da0f0aabc331814
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-09 11:44:48 +0500

    038: Cities

---
+++ cities/index.js
@@ -0,0 +1,6 @@
+function describe_city(city, country) {
+    return "".concat(city, " is in ").concat(country);
+}
+console.log(describe_city("Dubai", "UAE"));
+console.log(describe_city("Islamabad", "Pakistan"));
+console.log(describe_city("Istanbul", "Turkiye"));
---
+++ cities/index.ts
@@ -0,0 +1,7 @@
+function describe_city(city: string, country: string): string {
+    return `${city} is in ${country}`;
+}
+
+console.log(describe_city("Dubai", "UAE"));
+console.log(describe_city("Islamabad", "Pakistan"));
+console.log(describe_city("Istanbul", "Turkiye"));
