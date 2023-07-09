81feb69aff9c06d68d037bf1f368342fa319677b
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-09 11:33:40 +0500

    036: T-Shirt

---
+++ t-shits/index.js
@@ -0,0 +1,4 @@
+function make_shirt(size, msg) {
+    return "Size ".concat(size, ", Message: ").concat(msg);
+}
+console.log(make_shirt('XL', 'Hello World'));
---
+++ t-shits/index.ts
@@ -0,0 +1,6 @@
+function make_shirt(size :string, msg:string) : string {
+    
+    return `Size ${size}, Message: ${msg}`;
+}
+
+console.log(make_shirt('XL', 'Hello World'));
