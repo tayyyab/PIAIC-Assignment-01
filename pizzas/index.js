0c70ff196a909ce2e845d5f239167bcac5e78f80
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-09 10:30:18 +0500

    034: Pizzas

---
+++ pizzas/index.js
@@ -0,0 +1,12 @@
+var pizzas = [
+    "Chicken Suprime",
+    "Chicken Tikka",
+    "Pepperoni"
+];
+for (var _i = 0, pizzas_1 = pizzas; _i < pizzas_1.length; _i++) {
+    var pizza = pizzas_1[_i];
+    console.log("I like ".concat(pizza, " pizza."));
+}
+console.log("Recenly I like eating pizza.");
+console.log("I eat it once a week.");
+console.log("I realy love pizza.");
---
+++ pizzas/index.ts
@@ -0,0 +1,13 @@
+let pizzas: string[] = [
+    "Chicken Suprime",
+    "Chicken Tikka",
+    "Pepperoni"
+];
+
+for (let pizza of pizzas) {
+    console.log(`I like ${pizza} pizza.`);  
+}
+
+console.log(`Recenly I like eating pizza.`);
+console.log(`I eat it once a week.`);
+console.log(`I realy love pizza.`);
