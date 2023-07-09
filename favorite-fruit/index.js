34c754b07a58298dce8d2e9ef853474a5a253d11
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-09 09:39:21 +0500

    029: Favorite Fruit

---
+++ favorite-fruit/index.js
@@ -0,0 +1,26 @@
+var my_favorite_fruits = [
+    "mango",
+    "watermalon",
+    "grapes"
+];
+if (my_favorite_fruits.indexOf("mango") > -1) {
+    console.log("You really like Mango");
+}
+if (my_favorite_fruits.indexOf("watermalon") > -1) {
+    console.log("You really like Watermalon");
+}
+if (my_favorite_fruits.indexOf("grapes") > -1) {
+    console.log("You really like Grapes");
+}
+if (!(my_favorite_fruits.indexOf("strabarry") > -1)) {
+    console.log("You do not Strabarry");
+}
+if (!(my_favorite_fruits.indexOf("avacado") > -1)) {
+    console.log("You do not Avacado");
+}
+if (!(my_favorite_fruits.indexOf("banana") > -1)) {
+    console.log("You do not Banana");
+}
+if (my_favorite_fruits.includes("mango")) {
+    console.log("You really like Mango");
+}
