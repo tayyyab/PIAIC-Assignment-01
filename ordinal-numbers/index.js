569319a2cb0d0d5b182bb7c18e0673953c1c8c39
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-09 10:19:17 +0500

    033: Ordinal Numbers

---
+++ ordinal-numbers/index.js
@@ -0,0 +1,17 @@
+var numbers = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9
+];
+for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] == 1) {
+        console.log("".concat(numbers[i], "st"));
+    }
+    else if (numbers[i] == 2) {
+        console.log("".concat(numbers[i], "nd"));
+    }
+    else if (numbers[i] == 3) {
+        console.log("".concat(numbers[i], "rd"));
+    }
+    else {
+        console.log("".concat(numbers[i], "th"));
+    }
+}
