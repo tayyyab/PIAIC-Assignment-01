caf28ed3634144cc30c3add990507594c1913a5e
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-09 11:38:34 +0500

    037: Large Shirts

--- t-shits/index.js
+++ t-shits/index.js
@@ -1,4 +1,15 @@
 function make_shirt(size, msg) {
+    if (size === void 0) { size = "L"; }
+    if (msg === void 0) { msg = "I love TypeScript"; }
     return "Size ".concat(size, ", Message: ").concat(msg);
+    // switch(size) {
+    //     case 'L':
+    //     case 'M':
+    //         return `Size ${size}, Message: ${msg}`;
+    //     default:
+    //         return `Size ${size}, Message: ${msg}`;
+    // }
 }
+console.log(make_shirt('L'));
+console.log(make_shirt('M'));
 console.log(make_shirt('XL', 'Hello World'));
--- t-shits/index.ts
+++ t-shits/index.ts
@@ -1,6 +1,14 @@
-function make_shirt(size :string, msg:string) : string {
-    
+function make_shirt(size :string = "L", msg:string = "I love TypeScript") : string {
     return `Size ${size}, Message: ${msg}`;
+    // switch(size) {
+    //     case 'L':
+    //     case 'M':
+    //         return `Size ${size}, Message: ${msg}`;
+    //     default:
+    //         return `Size ${size}, Message: ${msg}`;
+    // }
 }
 
+console.log(make_shirt('L'));
+console.log(make_shirt('M'));
 console.log(make_shirt('XL', 'Hello World'));
