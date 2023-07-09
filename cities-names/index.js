6e3d6446f5b87cb37cd8cdfca9a9936897fbaafe
Muhammad Tayyab <mtbahaudin@taifco.net>
2023-07-09 11:48:39 +0500

    039: City Names

---
+++ cities-names/index.js
@@ -0,0 +1,6 @@
+function city_country(city, country) {
+    return "".concat(city, ", ").concat(country);
+}
+console.log(city_country("Dubai", "UAE"));
+console.log(city_country("Islamabad", "Pakistan"));
+console.log(city_country("Istanbul", "Turkiye"));
---
+++ cities-names/index.ts
@@ -0,0 +1,7 @@
+function city_country(city: string, country:string): string {
+    return `${city}, ${country}`;
+}
+
+console.log(city_country("Dubai", "UAE"));
+console.log(city_country("Islamabad", "Pakistan"));
+console.log(city_country("Istanbul", "Turkiye"));
